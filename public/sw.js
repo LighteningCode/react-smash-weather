48d5608586512fc25275cddaa54ea043ec29cc83
Elvis Agbesi <elvisagbesi@gmail.com>
2021-01-09 15:35:34 +0000

    chore: :construction: Added service worker file

---
+++ public/sw.js
@@ -0,0 +1,16 @@
+console.log('Hello from service worker')
+
+
+
+// self.addEventListener('activate', async () => {
+//     // This will be called only once when the service worker is activated.
+//     try {
+//         const options = {
+
+//         }
+//         const subscription = await self.registration.pushManager.subscribe(options)
+//         console.log(JSON.stringify(subscription))
+//     } catch (err) {
+//         console.log('Error', err)
+//     }
+// })
